42d0f404bb81181743610ee114cabdc83ccbee73
Somesh Mahendra <mahendrasomesh1491@gmail.com>
2021-01-05 22:32:36 +0530

    *Modify MemorySnap.h

--- include/MemorySnap.h
+++ include/MemorySnap.h
@@ -11,34 +11,13 @@
 #define new DBG_NEW
 #endif
 
+#ifdef _DEBUG
 
-bool isRecordingStarted = false;
-_CrtMemState mem_old;
-_CrtMemState mem_new;
-_CrtMemState mem_diff;
+void InitMemSnap();
+void MemRecordStart();
+void MemRecordStop();
+void DisplayMemStatus();
+
+#endif // !_DEBUG
 
 
-inline void InitMemSnap()
-{
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-}
-
-inline void MemRecordStart()
-{
-	assert(!isRecordingStarted);
-
-	_CrtMemCheckpoint(&mem_old);
-}
-
-inline void MemRecordStop()
-{
-	_CrtMemCheckpoint(&mem_new);
-}
-
-inline void DisplayMemStatus()
-{
-	if (_CrtMemDifference(&mem_diff, &mem_old, &mem_new))
-	{
-		std::cout << "Memory Leaked: " << mem_diff.lTotalCount << " Bytes" << std::endl;
-	}
-}
--- src/Main.cpp
+++ src/Main.cpp
@@ -8,45 +8,20 @@ int main()
 
 	MemRecordStart();
 	{
-		Matrix m1 = CreateMatrix(3, 4);
-		Matrix m2 = CreateMatrix(4, 4);
+		Matrix m1 = GetRandomMatrix(3, 4);
+		Matrix m2 = GetRandomMatrix(4, 4);
 		VectorN v1 = GetRandomVector(4);
-		
-		float arr1[] = { 2, 5, 3, 1 };
-		float arr2[] = { 3, 9, 6, 2 };
-		float arr3[] = { 4, 2, 1, 4 };
+		VectorN v2 = GetRandomVector(4);
 
-		SetRow(0, m1, arr1);
-		SetRow(1, m1, arr2);
-		SetRow(2, m1, arr3);
-
-		float arr11[] = { 2, 5, 3, 1};
-		float arr12[] = { 3, 9, 6, 2};
-		float arr13[] = { 4, 2, 1, 3};
-		float arr14[] = { 2, 1, 9, 4};
-
-		SetColumn(0, m2, arr11);
-		SetColumn(1, m2, arr12);
-		SetColumn(2, m2, arr13);
-		SetColumn(3, m2, arr14);
-
-		print(m1);
-		//printf("\n");
-		//print(v1);
-		//printf("\n");
-		//VectorN v2 = m1 * v1;
-		//print(v2);
+		Matrix m3 = m1 * m2;
+		VectorN v3 = v1 * m2;
 
 		DeleteVector(v1);
-		//DeleteVector(v2);
+		DeleteVector(v2);
+		DeleteVector(v3);
+		DeleteMatrix(m3);
 		DeleteMatrix(m1);
 		DeleteMatrix(m2);
-			/*VectorN v1 = GetRandomVector(4);
-			//print(v1);
-			DeleteVector(v1);
-			Matrix m1 = CreateMatrix(3, 4);
-			//print(m1);
-			DeleteMatrix(m1);*/
 	}
 	MemRecordStop();
 	DisplayMemStatus();
