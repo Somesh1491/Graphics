52b8e7aec2b4fa9a4d5e4ded5df67b66fb23019a
Somesh Mahendra <mahendrasomesh1491@gmail.com>
2021-01-17 17:58:09 +0530

    *Modify Primitive Assembly to calculate and store nessesary parameters to draw line. *Added Rasterizer Component.

--- src/Graphics/PrimitiveAssembly.cpp
+++ src/Graphics/PrimitiveAssembly.cpp
@@ -3,6 +3,8 @@
 extern x_transformedVertexBuffer* transformedVertexBuffer;
 extern x_Geometry currentGeometry;
 
+Primitive_Line* primitiveLine;
+
 void LinePrimitiveBlock();
 
 void PrimitiveAssemblyBlock()
@@ -19,4 +21,17 @@ void PrimitiveAssemblyBlock()
 
 void LinePrimitiveBlock()
 {
+	int lineCount = transformedVertexBuffer->vertexCount / 2;
+
+	primitiveLine = (Primitive_Line*)malloc(sizeof(Primitive_Line) * lineCount);
+	
+	for (int i = 0; i < lineCount; i++)
+	{
+		x_point2 p1 = transformedVertexBuffer->transformedVertexBufferPtr[i*2];
+		x_point2 p2 = transformedVertexBuffer->transformedVertexBufferPtr[i*2 + 1];
+
+		primitiveLine->dy = p2.y - p1.y;
+		primitiveLine->dx = p2.x - p1.x;
+		primitiveLine->d = 2 * primitiveLine->dy - primitiveLine->dx;
+	}
 }
